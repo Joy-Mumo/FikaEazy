465542af45db53c1504570625606ef2e35e9bf9a
Jared Omari <moindi.jared@gmail.com>
2018-10-05 09:35:59 +0300

    Basic script to run on page

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function tripOptions() {
+  var currentLocation = $("#currentLocation").find(":selected").text();
+  var destination = $("#destination").find(":selected").text();
+
+
+if((currentLocation="Hilton Hotel") && (destination="Moringa School")); {
+      message.innerHTML="fare is 50ksh,estimated time is 45minutes,stage is railways";
+      alert("fare is 50ksh")
+      alert("estimated time is 45minutes")
+      alert("stage is prestige")
+}
+if((currentLocation="Hilton Hotel") && (destination="Two Rivers Mall")); {
+  message.innerHTML="fare is 70ksh,estimated time is 40minutes,stage is Odeon";
+  alert("fare is 70ksh")
+  alert("estimated time is 40minutes")
+  alert("stage is Odeon")
+}
+if((currentLocation="Hilton Hotel") && (destination="Nakumatt Galleria")); {
+  message.innerHTML="fare is 80ksh,estimated time is 60minutes,stage is Afya House";
+  alert("fare is 80ksh")
+  alert("estimated time is 60minutes")
+  alert("stage is prestige")
+}
+if((currentLocation="Hilton Hotel") && (destination="Juja")); {
+  message.innerHTML="fare is 60ksh,estimated time is 60minutes,stage is Odeon";
+  alert("fare is 60ksh")
+  alert("estimated time is 60minutes")
+  alert("stage is Odeon")
+}
+
+#tripsummary {
+  display: none;
+  {
+
