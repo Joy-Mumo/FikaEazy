d3ce12597bbdb98fdfc147082c95e21ea9b6b4e9
Joy Vivika <joiemumo@gmail.com>
2018-10-05 12:23:13 +0300

    change carousel text font style

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,23 @@
-function tripOptions() {
-  var currentLocation = $("#currentLocation").find(":selected").text();
-  var destination = $("#destination").find(":selected").text();
-  var modeOfTransport = $("#transportmode").find(":selected").text();
+$(document).ready(function(){
 
-  if((currentLocation=="Lang'ata") && (destination=="Two Rivers Mall")); {
-        message.innerHTML="Fare:Ksh.200,Time estimate:1hour,Stage:Prestige";
-        // alert("Fare:Ksh.200")
-        // alert("Time estimate:1hour")
-        // alert("Stage:Prestige")
+   $("#myCarousel").carousel();
 
-  }
-  // else{
-  //   message.innerHTML="Route is not available current";
-  // }
+   
+   $(".item1").click(function(){
+       $("#myCarousel").carousel(0);
+   });
+   $(".item2").click(function(){
+       $("#myCarousel").carousel(1);
+   });
+   $(".item3").click(function(){
+       $("#myCarousel").carousel(2);
+   });
 
-if((currentLocation=="Lang'ata") && (destination="Galleria")); {
-        message.innerHTML="Fare:Ksh.400,Time estimate:1hour,Stage:Prestige";
-        alert("Fare:Ksh.400")
-        alert("Time estimate:1hour")
-        alert("Stage:Prestige")
-      }
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Galleria"); && (transportmode="Matatu")); {
-          message.innerHTML="Fare:Ksh.60,Time estimate:30minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.60")
-          alert("Time estimate:30minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation=="Lang'ata") && (destination=="Galleria") && (transportmode=="Motorbike")); {
-          message.innerHTML="Fare:Ksh.150,Time estimate:20minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.150")
-          alert("Time estimate:20minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation=="Lang'ata") && (destination=="Galleria") && (transportmode=="Taxi")); {
-          message.innerHTML="Fare:Ksh.150,Time estimate:20minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.300")
-          alert("Time estimate:40minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Moringa School") && (transportmode="Matatu")); {
-          message.innerHTML="Fare:Ksh.80,Time estimate:40minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.80")
-          alert("Time estimate:40minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Moringa School") && (transportmode="Taxi")); {
-          message.innerHTML="Fare:Ksh.500,Time estimate:20minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.500")
-          alert("Time estimate:40minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Moringa School") && (transportmode="Motorbike")); {
-          message.innerHTML="Fare:Ksh.150,Time estimate:40minutes,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.150")
-          alert("Time estimate:40minutes")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Juja") && (transportmode="Motorbike")); {
-          message.innerHTML="Fare:Ksh.300,Time estimate:1hour,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.300")
-          alert("Time estimate:1hour")
-          alert("Stage:Uhuru Gardens")
-
-  }
-  if((currentLocation="Lang'ata") && (destination="Juja") && (transportmode="Taxi")); {
-          message.innerHTML="Fare:Ksh.300,Time estimate:1hour,Stage:Uhuru Gardens";
-          alert("Fare:Ksh.500")
-          alert("Time estimate:1hour")
-          alert("Stage:Uhuru Gardens")
-
-  }
-    event.preventDefault();
-  }
+   // Enable Carousel Controls
+   $(".left").click(function(){
+       $("#myCarousel").carousel("prev");
+   });
+   $(".right").click(function(){
+       $("#myCarousel").carousel("next");
+   });
+});
